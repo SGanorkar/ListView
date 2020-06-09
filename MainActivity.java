7694c54afa1f1cf6987b8d00994add72c041a03c
SGanorkar <63590064+SGanorkar@users.noreply.github.com>
2020-06-09 21:09:17 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.gdrivebackup;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -25,7 +33,9 @@ public class MainActivity extends AppCompatActivity
 
     EditText searchText;
 
-    CustomAdapter adapter;
+    SimpleAdapter adapter;
+
+    LinkedHashMap<String, String> nameContact = new LinkedHashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -70,15 +80,34 @@ public class MainActivity extends AppCompatActivity
 
         while(result.moveToNext())
         {
-            custIdList.add(result.getString(0));
-            custNameList.add(result.getString(1));
+            String custId = result.getString(0);
+            custIdList.add(custId);
+
+            String custName = result.getString(1);
+            custNameList.add(custName);
+
+            nameContact.put(custId, custName);
         }
 
     }
 
     public void showList()
     {
-        adapter = new CustomAdapter(this, custIdList, custNameList);
+        //adapter = new CustomAdapter(this, custIdList, custNameList);
+        List<LinkedHashMap<String, String>> listItems = new ArrayList<>();
+        adapter = new SimpleAdapter(this, listItems, R.layout.item_subitem, new String[]{"ID", "Name"}, new int[]{R.id.sampleCustId, R.id.sampleCustName});
+
+        Iterator it = nameContact.entrySet().iterator();
+
+        while (it.hasNext())
+        {
+            LinkedHashMap<String, String> nameContactMap = new LinkedHashMap<>();
+            Map.Entry pair = (Map.Entry) it.next();
+            nameContactMap.put("ID", pair.getKey().toString());
+            nameContactMap.put("Name", pair.getValue().toString());
+            listItems.add(nameContactMap);
+        }
+
         listView.setAdapter(adapter);
     }
 
@@ -98,7 +127,7 @@ public class MainActivity extends AppCompatActivity
                 @Override
                 public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                     adapter.getFilter().filter(charSequence);
-                    listView.invalidateViews();
+                    //listView.invalidateViews();
                 }
 
                 @Override
@@ -121,9 +150,17 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
             {
-                Toast.makeText(MainActivity.this, custNameList.get(i), Toast.LENGTH_SHORT).show();
+                String requiredId = null;
+
+                Pattern p = Pattern.compile("ID=(.*?),");
+                Matcher m = p.matcher(adapter.getItem(i).toString());
+
+                while(m.find())
+                {
+                    requiredId = m.group(1);
+                }
+                Log.i("Item", requiredId);
             }
         });
     }
-
 }
